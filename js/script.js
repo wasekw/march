ffe4827dd58bc6aa8862f4d7e14bcde5769cd04e
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-03-08 17:27:15 +0200

    Added eslint all configuration

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-var number;
-number = 10;
-alert(number);
 
-const pi = 3.14;
-pi = 10;
+const arr = [2, 4, 5];
 
 
+arr.push(3);
+console.log(arr);
 
+
+let myName = 'Vasiliy';
+
+let bool = true;
+
+let nothing = null;
+let dog = undefined;
+
+let obj = {
+    mass: 100,
+    form: 'sercle',
+    quantity: [1, 3, 5, 7, 898],
+};
+console.log(dog);
